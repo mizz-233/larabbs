9c83abc70deaec341df72ad7b4ab9a9ae3266ba7
mizz-233 <3409133605@qq.com>
2024-05-31 14:05:12 +0800

    修复了邮箱认证找不到对应路由的问题

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\VerificationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Auth;
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', [PagesController::class, 'root'])->name('root');
+Route::get('/', [PagesController::class, 'root'])->name('root')->middleware('verified');;
 
 Auth::routes();
+
+// Email 认证相关路由
+Route::get('email/verify', [VerificationController::class,'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [VerificationController::class,'verify'])->name('verification.verify');
+Route::post('email/resend', [VerificationController::class,'resend'])->name('verification.resend');
